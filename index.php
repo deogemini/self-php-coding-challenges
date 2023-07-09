4954629fa2f6c02ee45da74c401dca1e9751b24a
Deogratias Gemini <geminideogratias8@gmail.com>
2023-07-10 01:28:34 +0300

    initial commit with initial code

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+// write function that takes two numbers as arguments and returns their sum.
+
+// Examples
+// addition(3, 2) ➞ 5
+
+// addition(-3, -6) ➞ -9
+
+// addition(7, 3) ➞ 10
+
+function addition($a, $b){
+    return $a + $b;
+}
+
+
+?>
